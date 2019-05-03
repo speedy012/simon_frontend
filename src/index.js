9a73961dd51bc8a5bcfd0c5b1f196d2495d86f67
Herman Rivas <hrivas5726@aol.com>
2019-05-03 15:22:54 -0400

    clean code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+
+********************************************************************************
+//DOM nodes
+********************************************************************************
+
 const gameCont = document.getElementById("game-container");
+const red = document.getElementById('red')
+const blue = document.getElementById('blue')
+const green = document.getElementById('green')
+const yellow = document.getElementById('yellow')
+
+
+********************************************************************************
+//Eventlisteners
+********************************************************************************
+
+
+// gameCont.addEventListener
+
+
+
+
+********************************************************************************
+//Functions
+********************************************************************************
